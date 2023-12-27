697698e2885548f3cca9e038985e38e1548bb1cd
Eduard Leal <130890133+EduardMaLH@users.noreply.github.com>
2023-12-27 10:00:35 -0500

    Update PokemonRepository.java

--- src/main/java/com/example/demo/repositories/PokemonRepository.java
+++ src/main/java/com/example/demo/repositories/PokemonRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
+public interface PokemonRepository extends JpaRepository<Pokemon,Integer> {
     List<Pokemon> findByTipoPokemon_Descripcion(String tipo);
+    static List<Pokemon> findByEntrenador(Entrenador entrenador) {
+		
+		return null;
+	}
+    Optional<Pokemon> findByUuid(String uuid);
 }
